9d3f4aef290ca8a140f8baa5d32b60ff8129601f
Gustavo-Crnts <gustavo.he.fe.santos@gmail.com>
2024-08-16 19:49:55 -0300

    first commit

---
+++ PamNotes/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace PamNotes
+{//nome desse arquivo é "CodeBehind" 
+    public partial class MainPage : ContentPage
+    {
+        string filePath = Path.Combine (FileSystem.AppDataDirectory, "guga.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(filePath))
+            {
+                {
+                    EditorSalvar.Text = File.ReadAllText(filePath);
+                }
+
+            }
+        }
+        //Para salvar em um arquivo tenho que fazer:
+        // 1º Criar o arquivo
+        // 2º Pegar o texto e armazenar em uma variável
+        //3º Gravar o conteúdo da variável de um arquivo
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string texto = EditorSalvar.Text;
+
+            Exibir.Text = texto;
+            File.WriteAllText(filePath, texto);
+            DisplayAlert("Operação concluída", "Arquivo salvo com sucesso!", "Ok");
+
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+           
+                string texto = EditorSalvar.Text;
+
+            Exibir.Text = " ";
+             
+            EditorSalvar.Text = "Sua anotação foi apagada com sucesso! ";
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                DisplayAlert("Operação concluída!", "Arquivo foi apagado com sucesso!", "Ok");
+                EditorSalvar.Text = " ";
+            }
+
+
+            else
+            {
+                DisplayAlert("Alerta", "Arquivo não encontrado", "Ok");
+            }
+
+        }
+
+
+    }
+
+}
